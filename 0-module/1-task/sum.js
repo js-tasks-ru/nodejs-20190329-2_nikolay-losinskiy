8cb3215831c669c1d89474337bc4f73c3e08aba4
Mykola Losinskyi <nick.losinskii@gmail.com>
2019-04-02 12:23:24 +0300

    added solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+    if (!isNumeric(a) || !isNumeric(b)) {
+        throw new TypeError('TypeError');
+    }
+
+    return a + b;
+}
+
+function isNumeric(num) {
+    return isFinite(num) && !isNaN(parseFloat(num));
 }
 
 module.exports = sum;
