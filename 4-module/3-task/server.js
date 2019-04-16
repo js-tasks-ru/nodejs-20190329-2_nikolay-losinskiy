60c8aff79701f5be272af63e5d95e5de5e9456f7
Mykola Losinskyi <nick.losinskii@gmail.com>
2019-04-16 09:09:29 +0300

    added solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,12 +12,36 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
-
+      deleteFile(req, res, filepath);
       break;
 
     default:
       res.statusCode = 501;
       res.end('Not implemented');
+  }
+
+  function deleteFile(req, res, filepath) {
+    if (pathname.indexOf('/') !== -1) {
+      res.statusCode = 400;
+      res.end();
+      return;
+    }
+
+    fs.unlink(filepath, (error) => {
+
+      if (error) {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+        } else {
+          res.statusCode = 500;
+        }
+        res.end();
+      }
+
+
+      res.statusCode = 200;
+      res.end();
+    })
   }
 });
 
