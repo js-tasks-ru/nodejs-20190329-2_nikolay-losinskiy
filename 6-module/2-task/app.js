6f0de236ad5c3cca880604894273c1f3216bbf26
Mykola Losinskyi <nick.losinskii@gmail.com>
2019-05-16 23:39:41 +0300

    added solution for 6-module 2-task

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -1,5 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const User = require('./models/User');
+const mongoose = require('mongoose');
 
 const app = new Koa();
 
@@ -23,23 +25,67 @@ app.use(async (ctx, next) => {
 const router = new Router();
 
 router.get('/users', async (ctx) => {
-
+  ctx.body = await User.find({});
 });
 
 router.get('/users/:id', async (ctx) => {
+  try {
+    const id = ctx.params.id;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      ctx.status = 400;
+      return;
+    }
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      ctx.status = 404;
+      return;
+    }
+
+    ctx.body = user;
+  } catch (err) {
+    ctx.status = 500;
+  }
 });
 
 router.patch('/users/:id', async (ctx) => {
-
+  try {
+    const id = ctx.params.id;
+    ctx.body = await User.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+      runValidators: true,
+    });
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = {errors: {email: err.errors.email.message}};
+  }
 });
 
 router.post('/users', async (ctx) => {
-
+  try {
+    ctx.body = await User.create(ctx.request.body);
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = {errors: {email: err.errors.email.message}};
+  }
 });
 
 router.delete('/users/:id', async (ctx) => {
+  try {
+    const id = ctx.params.id;
+    const user = await User.findByIdAndDelete(id);
 
+    if (!user) {
+      ctx.status = 404;
+      return;
+    }
+
+    ctx.status = 200;
+  } catch (err) {
+    ctx.status = 500;
+  }
 });
 
 app.use(router.routes());
