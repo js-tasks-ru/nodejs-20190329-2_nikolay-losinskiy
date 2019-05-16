9565ceb94788d9b8d5b9c5f1d36712c5b00ebb4f
Mykola Losinskyi <nick.losinskii@gmail.com>
2019-05-16 20:08:30 +0300

    added solution for 6-module 1-task

--- 6-module/1-task/User.js
+++ 6-module/1-task/User.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [
+      {
+        validator(value) {
+          return /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/.test(value);
+        },
+      },
+    ],
+    lowercase: true,
+    trim: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+  },
+}, {
+  timestamps: true,
+});
 
 module.exports = mongoose.model('User', schema);
